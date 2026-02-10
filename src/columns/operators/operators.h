2bc17d86eaf8ca1a505b1471c3ee769364ba04ba
dub-otrezkov <556danbrown556@gmail.com>
2026-02-10 15:19:43 +0300

    added-sum-operator

--- src/columns/operators/operators.h
+++ src/columns/operators/operators.h
@@ -1,105 +1,11 @@
 #pragma once
 
+#include "io_operators.h"
+#include "arithmetic_operators.h"
+
 #include "../types/types.h"
 
 namespace JFEngine {
-
-class OPrintIth {
-public:
-    static std::string Exec(Ti8Column& col, ui64 i) {
-        auto res = col.GetData()[i];
-        return std::to_string(res);
-    }
-    static std::string Exec(Ti16Column& col, ui64 i) {
-        auto res = col.GetData()[i];
-        return std::to_string(res);
-    }
-
-    static std::string Exec(Ti32Column& col, ui64 i) {
-        auto res = col.GetData()[i];
-        return std::to_string(res);
-    }
-
-    static std::string Exec(Ti64Column& col, ui64 i) {
-        auto res = col.GetData()[i];
-        return std::to_string(res);
-    }
-
-    static std::string Exec(TDoubleColumn& col, ui64 i) {
-        auto res = col.GetData()[i];
-        auto ans = std::to_string(res);
-        while (!ans.empty() && ans.back() == '0') {
-            ans.pop_back();
-        }
-        if (ans.back() == '.') {
-            ans.pop_back();
-        }
-        return ans;
-    }
-
-    static std::string Exec(TDateColumn& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return PrintDate(j);
-    }
-
-    static std::string Exec(TTimestampColumn& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return PrintTimestamp(j);
-    }
-
-    static std::string Exec(TStringColumn& col, ui64 i) {
-        auto res = col.GetData()[i];
-        return res;
-    }
-};
-
-class OJFPrintIth {
-public:
-    static std::string Exec(Ti8Column& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return I8ToJFStr(j);
-    }
-
-    static std::string Exec(Ti16Column& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return I16ToJFStr(j);
-    }
-
-    static std::string Exec(Ti32Column& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return I32ToJFStr(j);
-    }
-
-    static std::string Exec(Ti64Column& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return I64ToJFStr(j);
-    }
-
-    static std::string Exec(TDoubleColumn& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return DoubleToJFStr(j);
-    }
-
-    static std::string Exec(TDateColumn& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return I16ToJFStr(j.year) + I8ToJFStr(j.month) + I8ToJFStr(j.day);
-    }
-
-    static std::string Exec(TTimestampColumn& col, ui64 i) {
-        auto j = col.GetData()[i];
-        return I16ToJFStr(j.date.year) +
-               I8ToJFStr(j.date.month) +
-               I8ToJFStr(j.date.day) +
-               I8ToJFStr(j.hour) +
-               I8ToJFStr(j.minute) +
-               I8ToJFStr(j.second);
-    }
-
-    static std::string Exec(TStringColumn& col, ui64 i) {
-        auto res = col.GetData()[i];
-        return res;
-    }
-};
 
 template <typename TOperator, typename... Args>
 auto Do(TColumnPtr col, Args&&... args) {
---
+++ src/columns/tests/sum_test.cpp
@@ -0,0 +1,133 @@
+#include "../operators/operators.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <string_view>
+
+namespace JFEngine::Testing {
+
+struct OperatorsTest : testing::Test {};
+
+TEST_F(OperatorsTest, i64SumTest) {
+    {
+        std::vector<std::string> data = {"-1", "10", "9", "9"};
+        auto m = MakeColumn(data, TColumn::Ei64Column);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_FALSE(r.HasError());
+
+        ASSERT_TRUE(r.GetShared()->GetSize() == 1);
+        ASSERT_TRUE(r.GetShared()->GetType() == TColumn::Ei64Column);
+        auto res = std::dynamic_pointer_cast<Ti64Column>(r.GetShared());
+
+        EXPECT_EQ(res->GetData()[0], 27);
+    }
+}
+
+TEST_F(OperatorsTest, i32SumTest) {
+    {
+        std::vector<std::string> data = {"-1", "10", "9", "9"};
+        auto m = MakeColumn(data, TColumn::Ei32Column);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_FALSE(r.HasError());
+
+        ASSERT_TRUE(r.GetShared()->GetSize() == 1);
+        ASSERT_TRUE(r.GetShared()->GetType() == TColumn::Ei64Column);
+        auto res = std::dynamic_pointer_cast<Ti64Column>(r.GetShared());
+
+        EXPECT_EQ(res->GetData()[0], 27);
+    }
+}
+
+TEST_F(OperatorsTest, i16SumTest) {
+    {
+        std::vector<std::string> data = {"-1", "10", "9", "9"};
+        auto m = MakeColumn(data, TColumn::Ei16Column);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_FALSE(r.HasError());
+
+        ASSERT_TRUE(r.GetShared()->GetSize() == 1);
+        ASSERT_TRUE(r.GetShared()->GetType() == TColumn::Ei64Column);
+        auto res = std::dynamic_pointer_cast<Ti64Column>(r.GetShared());
+
+        EXPECT_EQ(res->GetData()[0], 27);
+    }
+}
+
+TEST_F(OperatorsTest, i8SumTest) {
+    {
+        std::vector<std::string> data = {"-1", "10", "9", "9"};
+        auto m = MakeColumn(data, TColumn::Ei32Column);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_FALSE(r.HasError());
+
+        ASSERT_TRUE(r.GetShared()->GetSize() == 1);
+        ASSERT_TRUE(r.GetShared()->GetType() == TColumn::Ei64Column);
+        auto res = std::dynamic_pointer_cast<Ti64Column>(r.GetShared());
+
+        EXPECT_EQ(res->GetData()[0], 27);
+    }
+}
+
+TEST_F(OperatorsTest, StringSumTest) {
+    {
+        std::vector<std::string> data = {"john", "*", "frusciante", "10"};
+        auto m = MakeColumn(data, TColumn::EStringColumn);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_FALSE(r.HasError());
+
+        ASSERT_TRUE(r.GetShared()->GetSize() == 1);
+        ASSERT_TRUE(r.GetShared()->GetType() == TColumn::EStringColumn);
+        auto res = std::dynamic_pointer_cast<TStringColumn>(r.GetShared());
+
+        EXPECT_EQ(res->GetData()[0], "john*frusciante10");
+    }
+}
+
+TEST_F(OperatorsTest, DateSumTest) {
+    {
+        std::vector<std::string> data = {"2022-02-24", "2025-14-02"};
+        auto m = MakeColumn(data, TColumn::EDateColumn);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_TRUE(r.HasError());
+    }
+}
+
+TEST_F(OperatorsTest, TimestampSumTest) {
+    {
+        std::vector<std::string> data = {"2022-02-24 05:19:10", "2025-14-02 11:09:01"};
+        auto m = MakeColumn(data, TColumn::ETimestampColumn);
+
+        ASSERT_FALSE(m.HasError());
+
+        auto r = Do<OSum>(m.GetShared());
+
+        ASSERT_TRUE(r.HasError());
+    }
+}
+
+} // namespace JFEngine::Testing
