0f47018131870833022b63fb879d3aaf0140f163
dub-otrezkov <556danbrown556@gmail.com>
2026-02-17 13:08:09 +0300

    first query exited

---
+++ src/workers/global_agregations/operators.cpp
@@ -0,0 +1,59 @@
+#include "operators.h"
+
+#include "columns/operators/operators.h"
+
+namespace JFEngine {
+
+void IAgregation::AddArg(std::shared_ptr<IAgregation>) {
+}
+
+Expected<IColumn> TColumnAgr::ReadRowGroup(ITableInput* inp) {
+    return inp->ReadColumn(name);
+}
+
+Expected<IColumn> TSumAgr::ReadRowGroup(ITableInput* inp) {
+    bool run = 1;
+    std::shared_ptr<IColumn> ans = nullptr;
+    inp->Reset();
+
+    while (run) {
+        auto [col, err] = arg->ReadRowGroup(inp);
+        if (err) {
+            if (Is<EofErr>(err)) {
+                run = 0;
+            } else {
+                return err;
+            }
+        }
+        auto sum = Do<OSum>(col);
+        if (sum.HasError()) {
+            return sum.GetError();
+        }
+
+        if (!ans) {
+            ans = sum.GetShared();
+        } else {
+            auto tmp = Do<OVerticalSum>(ans, sum.GetShared());
+
+            if (tmp.HasError()) {
+                return tmp.GetError();
+            }
+
+            ans = tmp.GetShared();
+        }
+
+        inp->MoveCursor(1);
+    }
+    Expected<IColumn> ret(std::move(ans), MakeError<EofErr>());
+    return ret;
+}
+
+TColumnAgr::TColumnAgr(std::string column_name) {
+    name = std::move(column_name);
+}
+
+void TSumAgr::AddArg(std::shared_ptr<IAgregation> to_sum) {
+    arg = to_sum;
+}
+
+} // namespace JFEngine
