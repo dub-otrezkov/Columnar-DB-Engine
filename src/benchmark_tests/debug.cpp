7abb14712c7d59ffe5c4f1203846b9e997062434
dbotr <556danbrown556@gmail.com>
2026-03-18 16:43:38 +0300

    tests

--- src/benchmark_tests/debug.cpp
+++ src/benchmark_tests/debug.cpp
@@ -20,15 +20,17 @@ ste,int16
 audun,string
 getaway,int8
 empty,string
+low,date
+beam,timestamp
 )"; // TODO add dates
-    std::string data = R"(1,2,josh,rip,4,laading,1,""
-3,4,john,rip,5,she needs him,1,dolores
-5,6,frusciante,forever,6,harvey,2,dolores
-7,8,klinghoffer,alive,6,"low,beam",2,""
-5,8,john,alive,8,dorothy,2,""
-1,4,klinghoffer,forever,1,if you know what's right,4,dolores2
-7,2,frusciante,forever,1,speed racer,2,dolores1
-3,7,josh,forever,1,cool with u,1,dolores1
+    std::string data = R"(1,2,josh,rip,4,laading,1,"",2023-05-15,2023-05-15 14:30:22
+3,4,john,rip,5,she needs him,1,dolores,2022-11-08,2021-11-08 09:45:17
+5,6,frusciante,forever,6,harvey,2,dolores,2022-03-27,2022-03-27 23:18:43
+7,8,klinghoffer,alive,6,"low,beam",2,"",2020-09-12,2020-09-12 07:52:09
+5,8,john,alive,8,dorothy,2,"",2024-01-30,2024-01-30 16:40:55
+1,4,klinghoffer,forever,1,if you know what's right,4,dolores2,2022-07-19,2019-07-19 11:33:41
+7,2,frusciante,forever,1,speed racer,2,dolores1,2022-12-03,2023-12-03 20:15:28
+3,7,josh,forever,1,cool with u,1,dolores1,2022-06-21,2022-06-21 05:08:14
 )";
 
     static constexpr ui64 iter = 50000;
@@ -71,6 +73,7 @@ empty,string
     }
 };
 
+
 void prolog(JFEngine::TExecutor& exec) {
     auto err = exec.ExecQuery("CREATE josh FROM scheme, data");
     if (err.HasError()) {
@@ -79,12 +82,12 @@ void prolog(JFEngine::TExecutor& exec) {
     ASSERT_FALSE(err.HasError());
 }
 
-TEST_F(BenchTest, _10) {
+TEST_F(BenchTest, _37) {
     
     JFEngine::TExecutor exec;
     prolog(exec);
     {
-        auto err = exec.ExecQuery("SELECT hers, SUM(what), COUNT(*) AS c, AVG(once), COUNT(DISTINCT(was)) FROM josh GROUP BY hers ORDER BY c DESC LIMIT 2");
+        auto err = exec.ExecQuery("SELECT was, COUNT(*) AS cnt FROM josh WHERE ste = 1 AND low <= '2022-12-31' AND low >= '2022-01-01' AND empty <> '' GROUP BY was ORDER BY cnt DESC LIMIT 10");
         if (err.HasError()) {
             std::cout << err.GetError() << std::endl;
         }
@@ -92,15 +95,14 @@ TEST_F(BenchTest, _10) {
     }
 
     std::cout << out_scheme->str() << std::endl;
+    std::cout << out_data->str() << std::endl;
 
-    EXPECT_EQ(out_scheme->str(), R"(hers,string
-SUM(what),int64
-c,int64
-AVG(once),double
-COUNT(DISTINCT(was)),int64
+    EXPECT_EQ(out_scheme->str(), R"(was,string
+cnt,int64
 )");
-    EXPECT_EQ(out_data->str(), R"(forever,800000,200000,4.75,3
-alive,600000,100000,8,2
+    EXPECT_EQ(out_data->str(), R"(frusciante,50000
+josh,50000
+klinghoffer,50000
 )");
 }
 
--- src/benchmark_tests/tests.cpp
+++ src/benchmark_tests/tests.cpp
@@ -24,12 +24,12 @@ low,date
 beam,timestamp
 )"; // TODO add dates
     std::string data = R"(1,2,josh,rip,4,laading,1,"",2023-05-15,2023-05-15 14:30:22
-3,4,john,rip,5,she needs him,1,dolores,2021-11-08,2021-11-08 09:45:17
+3,4,john,rip,5,she needs him,1,dolores,2022-11-08,2021-11-08 09:45:17
 5,6,frusciante,forever,6,harvey,2,dolores,2022-03-27,2022-03-27 23:18:43
 7,8,klinghoffer,alive,6,"low,beam",2,"",2020-09-12,2020-09-12 07:52:09
 5,8,john,alive,8,dorothy,2,"",2024-01-30,2024-01-30 16:40:55
-1,4,klinghoffer,forever,1,if you know what's right,4,dolores2,2019-07-19,2019-07-19 11:33:41
-7,2,frusciante,forever,1,speed racer,2,dolores1,2023-12-03,2023-12-03 20:15:28
+1,4,klinghoffer,forever,1,if you know what's right,4,dolores2,2022-07-19,2019-07-19 11:33:41
+7,2,frusciante,forever,1,speed racer,2,dolores1,2022-12-03,2023-12-03 20:15:28
 3,7,josh,forever,1,cool with u,1,dolores1,2022-06-21,2022-06-21 05:08:14
 )";
 
@@ -744,6 +744,30 @@ c,int64
 dolores,100000
 ,100000
 ,100000
+)");
+}
+
+TEST_F(BenchTest, _37_38) {
+    
+    JFEngine::TExecutor exec;
+    prolog(exec);
+    {
+        auto err = exec.ExecQuery("SELECT was, COUNT(*) AS cnt FROM josh WHERE ste = 1 AND low <= '2022-12-31' AND low >= '2022-01-01' AND empty <> '' GROUP BY was ORDER BY cnt DESC LIMIT 10");
+        if (err.HasError()) {
+            std::cout << err.GetError() << std::endl;
+        }
+        ASSERT_FALSE(err.HasError());
+    }
+
+    // std::cout << out_scheme->str() << std::endl;
+    // std::cout << out_data->str() << std::endl;
+
+    EXPECT_EQ(out_scheme->str(), R"(was,string
+cnt,int64
+)");
+    EXPECT_EQ(out_data->str(), R"(frusciante,50000
+josh,50000
+klinghoffer,50000
 )");
 }
 
