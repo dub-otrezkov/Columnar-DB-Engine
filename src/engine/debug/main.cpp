93d9fc762a1b217a06046c22584a6591da17f682
dbotr <556danbrown556@gmail.com>
2026-04-04 18:27:39 +0300

    fin

--- src/engine/debug/main.cpp
+++ src/engine/debug/main.cpp
@@ -1,20 +1,2 @@
-#include "engine/engine.h"
-
-#include "workers/selector/selector.h"
-
-#include "utils/logger/logger.h"
-
-#include <iostream>
-#include <sstream>
-#include <fstream>
-
-// (cd ../../build/engine/debug; make debug_engine); ../../build/engine/debug/debug_engine
-
-using namespace JFEngine;
-
 int main() {
-    auto scheme = std::make_shared<std::fstream>("scheme.csv");
-    auto data = std::make_shared<std::fstream>("data.csv");
-    auto [eng, err] = MakeEngineFromCSV(scheme, data);
-
 }
