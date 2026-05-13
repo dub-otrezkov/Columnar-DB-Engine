5bb0eb65121a930bd839b805b04d989e8e476e2c
vyach <556danbrown556@gmail.com>
2026-05-13 12:20:41 +0300

    logger

---
+++ src/utils/logger/logger.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace JfEngine {
+
+class TLogger {
+public:
+    static std::ostream* out_;
+
+    static inline void Write(
+        const char* file,
+        int line,
+        const void* obj,
+        const std::string& msg
+    ) {
+        if (!out_) {
+            return;
+        }
+        (*out_) << file << ":" << line << " " << obj << " " << msg << '\n';
+    }
+};
+
+} // namespace JfEngine
+
+#ifdef NDEBUG
+    #define JF_LOG(obj, msg) ((void)0)
+#else
+    #define JF_LOG(obj, msg)                                            \
+        do {                                                            \
+            std::stringstream jf_log_ss_;                               \
+            jf_log_ss_ << msg;                                          \
+            ::JfEngine::TLogger::Write(                                 \
+                __FILE__, __LINE__, (obj), jf_log_ss_.str()             \
+            );                                                          \
+        } while (0)
+#endif
